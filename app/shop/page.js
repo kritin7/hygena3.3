42188aadb5a3a028a32d89192c7a08d021f1009c
kritin7 <62542754+kritin7@users.noreply.github.com>
2025-12-15 17:29:45 +0530

    Update page.js

--- app/shop/page.js
+++ app/shop/page.js
@@ -17,7 +17,7 @@ export default function ShopPage() {
     {
       id: 1,
       name: "Hygena Helmet Deodorant",
-      image: "https://images.unsplash.com/photo-1649176154020-c695980078e8?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NDQ2NDF8MHwxfHNlYXJjaHwxfHxkZW9kb3JhbnQlMjBzcHJheXxlbnwwfHx8b3JhbmdlfDE3NTczMTY5NzR8MA&ixlib=rb-4.1.0&q=85",
+      image: "/images/1st.jpg",
       size: "100ml",
       duration: "30 Days Protection",
       price: 549,
