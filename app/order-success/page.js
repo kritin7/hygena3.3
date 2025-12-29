bc37af1dbe41ae1532c9d24576175ce957b5e41a
kritin7 <62542754+kritin7@users.noreply.github.com>
2025-12-29 22:54:16 +0530

    Update page.js

--- app/order-success/page.js
+++ app/order-success/page.js
@@ -79,7 +79,7 @@ export default function OrderSuccessPage() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '857758360478605');
+          fbq('init', '885932527303026'); 
           fbq('track', 'PageView');
         `}
       </Script>
