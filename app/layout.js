8d14690b99fdd399971b3bc03e4caf77e82c80c8
kritin7 <62542754+kritin7@users.noreply.github.com>
2025-12-30 17:18:29 +0530

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -27,40 +27,13 @@ export const metadata = {
   },
 }
 
+import MetaPixel from "./MetaPixel";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${montserrat.variable}`}>
-      <head>
-        {/* Meta Pixel Code - Initialize on all pages */}
-        <Script
-          id="meta-pixel-base"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '885932527303026');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=885932527303026&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-      </head>
       <body className="font-inter antialiased">
+        <MetaPixel />
         <SessionWrapper>
           <Navbar />
           <main className="min-h-screen">
@@ -71,5 +44,5 @@ export default function RootLayout({ children }) {
         </SessionWrapper>
       </body>
     </html>
-  )
+  );
 }
