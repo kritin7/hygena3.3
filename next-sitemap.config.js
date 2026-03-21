e75123b8f6d39009362111d35507800bc99f55ab
kritin7 <62542754+kritin7@users.noreply.github.com>
2026-03-21 23:58:24 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,21 +3,4 @@ module.exports = {
   siteUrl: 'https://www.hygena.in',
   generateRobotsTxt: true,
   sitemapSize: 5000,
-
-  exclude: [
-    '/auth/*',
-    '/checkout',
-    '/dashboard',
-    '/api/*',
-    '/order-success'
-  ],
-
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  },
 };
